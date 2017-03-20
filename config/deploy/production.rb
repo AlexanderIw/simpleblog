6af2f4b774bcd13b387e4fe1388d5c77e1596b1d
niquepg <l.w.alexander@hotmail.com>
2017-03-19 21:58:03 -0400

    attempt to deploy #7

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 set :stage, :production
-server '174.138.77.50', user: 'niquepg', roles: %w{web app}
+server '174.138.77.50', user: 'niquepg', roles: %w{web app}, port: 4093
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
